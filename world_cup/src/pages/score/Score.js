509aee25a1608e02ce157b5504b0f1b52801dd95
Dilixiati Yimuran <Emran.dilixiati@gmail.com>
2021-12-01 21:21:00 +0100

    simple css design for component Score,, on Header and match history

--- world_cup/src/pages/score/Score.js
+++ world_cup/src/pages/score/Score.js
@@ -11,7 +11,38 @@ export default function Score() {
                     World Cup Matches
                 </button>
             </header>
-            Score
+            <div className="page-score-main">
+                <div className="score-main-results">
+                    <div className="score-main-results-title">Results</div>
+                    <div className="score-main-results-scores">
+                        <div className="score-main-results-scores-each">
+                            <div className="results-each-section-title">
+                                <div>Home</div>
+                                <div>-</div>
+                                <div>Away</div>
+                            </div>
+                            <div className="results-each-section-number">
+                                <div>1</div>
+                                <div>2</div>
+                            </div>
+                            <div className="results-each-section-country">
+                                <div>Norway</div>
+                                <div>Sweden</div>
+                            </div>
+                            <div className="results-each-section-history">
+                                <div>History</div>
+                                <div className="results-each-section-country-history"> 
+                                    <div>11:35</div> <div>Goal</div> <div>Norway</div> 
+                                </div>
+                                <div className="results-each-section-country-history"> 
+                                    <div>30:16</div> <div>Goal</div> <div>Sweden</div> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="score-main-matches">Matches</div>
+            </div>
         </div>
     )
 }
