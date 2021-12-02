e64a1f0f5689ad36626edef56e391625303a516c
Dilixiati Yimuran <Emran.dilixiati@gmail.com>
2021-12-02 21:54:15 +0100

    Making comments to the code, made the world cup dashboard more dynamic, to be able to add more matches, and remove score if given mistake is made

--- world_cup/src/pages/score/Score.js
+++ world_cup/src/pages/score/Score.js
@@ -1,80 +1,119 @@
 import React, { useState, useEffect } from "react";
 import "./score.css";
-import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Score() {
-  const [matches, setMatches] = useState([
-    {
-      home: "Mexico",
-      away: "Canada",
-      homeScore: 0,
-      awayScore: 0,
-      totalScore: 0,
-      started: false,
-    },
-    {
-      home: "Spain",
-      away: "Brazil",
-      homeScore: 0,
-      awayScore: 0,
-      totalScore: 0,
-      started: false,
-    },
-    {
-      home: "Germany",
-      away: "France",
-      homeScore: 0,
-      awayScore: 0,
-      totalScore: 0,
-      started: false,
-    },
-    {
-      home: "Uruguay",
-      away: "Italy",
-      homeScore: 0,
-      awayScore: 0,
-      totalScore: 0,
-      started: false,
-    },
-    {
-      home: "Argentina",
-      away: "Australia",
-      homeScore: 0,
-      awayScore: 0,
-      totalScore: 0,
-      started: false,
-    },
-  ]);
+    // nested list of countries facing each other on World Cup
+    const [countries] = useState([
+        ["Mexico", "Canada"], 
+        ["Spain", "Brazil"], 
+        ["Germany", "France"], 
+        ["Uruguay", "Italy"],
+        ["Argentina", "Australia"]
+    ])
+    // state for creating matches, home and away team
+    const [homeTeamName, setHomeTeamName] = useState("")
+    const [awayTeamName, setAwayTeamName] = useState("")
+    
+    // Matches which has not yet finished.
+    const [matches, setMatches] = useState([]);
+    
+    // Results of matches finished
+    const [results, setResults] = useState([]);
 
-  const [results, setResults] = useState([]);
+    // Creates match objects on component open
+    useEffect(() => {
+        let createMatches = []
+        for (let i = 0; i < countries.length; i++) {
+            createMatches = [...createMatches, 
+                {
+                    home: countries[i][0],
+                    away: countries[i][1],
+                    homeScore: 0,
+                    awayScore: 0,
+                    totalScore: 0,
+                    started: false,
+                }
+            ]
+        }
+        setMatches(createMatches)
+    }, [countries])
+    
+    // I anticipated that the matches objects will come from an api, but I still made it possible to add matches on the website.
+    function addCountries(event) {
+		event.preventDefault();
+        setMatches([...matches,
+            {
+                home: homeTeamName,
+                away: awayTeamName,
+                homeScore: 0,
+                awayScore: 0,
+                totalScore: 0,
+                started: false
+            }
+        ])
+        setHomeTeamName("")
+        setAwayTeamName("")
+    }
 
+  // I made all the functionality in one function to make it easier to understand,
+  // however, it would be better option
   function endOrStartMatch(index, startOrFinish) {
-    let allMatches = [...matches];
+    let allMatches = [...matches]; // copying all matches not yet finished
+    // if start button is clicked, we change the state "started" on given index from parameter
     if (startOrFinish === "start") {
       allMatches[index].started = true;
-    } else {
+    }
+    // if end button is clicked, we (*):
+    else {
+      // * change state "started" to false for current match
       allMatches[index].started = false;
+      // saving the current game
       let currentMatch = allMatches[index];
+      // * deletes the current match from state "matches"
       allMatches.splice(index, 1);
-      const sortResultsByTotalScore = [currentMatch, ...results].sort(
-        (a, b) => a.totalScore > b.totalScore ? -1: 1
+      // * appending current match to front of results as sortResultsByTotalScore
+      // * reverse sorting sortResultsByTotalScore based on totalScore
+      const sortResultsByTotalScore = [currentMatch, ...results].sort((a, b) =>
+        a.totalScore > b.totalScore ? -1 : 1
       );
+      // updating state "results"
       setResults(sortResultsByTotalScore);
     }
+    // updating state "matches" not yet finished
     setMatches(allMatches);
   }
 
-  function addScore(index, side) {
+  // addScore function to +1 or -1 to either "side" (parameter) and +1 or -1 to totalScore
+  function addScore(index, side, addOrRemove) {
     let allMatches = [...matches];
-    allMatches[index][side + "Score"] += 1;
-    allMatches[index].totalScore += 1;
+    switch (addOrRemove) {
+        case 'add':
+            allMatches[index][side + "Score"] += 1
+            allMatches[index].totalScore += 1
+            break;
+        case 'remove':
+            if (allMatches[index][side + "Score"] !== 0) {
+                allMatches[index][side + "Score"] -= 1
+                allMatches[index].totalScore -= 1
+            }
+            break;
+        default:
+            break
+    }
     setMatches(allMatches);
   }
 
   return (
     <div className="page-score">
       <header className="page-score-header">
-        <button onClick={<Navigate to="/" />}>World Cup Matches</button>
+        <Link to="/">World Cup Matches</Link>
+        <form onSubmit={(event) => addCountries(event)}>
+            <p>Add matches</p>
+            <input type="text" required={true} id="home" value={homeTeamName} placeholder="Home Team" onChange={(event) => setHomeTeamName(event.target.value)}></input>
+            <input type="text" required={true} id="away" value={awayTeamName} placeholder="Away Team" onChange={(event) => setAwayTeamName(event.target.value)}></input>
+            <button type="submit">Create</button>
+        </form>
       </header>
       <div className="page-score-main">
         <div className="score-main-results">
@@ -129,16 +168,32 @@ export default function Score() {
                   <button
                     id="home"
                     disabled={!eachMatch.started}
-                    onClick={(event) => addScore(index, event.target.id)}
+                    onClick={(event) => addScore(index, event.target.id, "add")}
                   >
                     +1 Home
                   </button>
                   <button
                     id="away"
                     disabled={!eachMatch.started}
-                    onClick={(event) => addScore(index, event.target.id)}
+                    onClick={(event) => addScore(index, event.target.id, "add")}
                   >
                     +1 Away
+                  </button>
+                </div>
+                <div className="score-main-matches-each-functionality">
+                  <button
+                    id="home"
+                    disabled={!eachMatch.started}
+                    onClick={(event) => addScore(index, event.target.id, "remove")}
+                  >
+                    -1 Home
+                  </button>
+                  <button
+                    id="away"
+                    disabled={!eachMatch.started}
+                    onClick={(event) => addScore(index, event.target.id, "remove")}
+                  >
+                    -1 Away
                   </button>
                 </div>
                 {eachMatch.started && (
